c08b72d76a3965b7493249e45d003e2a1edbe47c
ekfomina <ek.fomina@inbox.ru>
2023-01-30 22:13:32 +0300

    django

---
+++ config/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config556 for config556 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config556.settings')
+
+application = get_asgi_application()
